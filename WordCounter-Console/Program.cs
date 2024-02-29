35dabafbd22d2b4a7bb7fe080c4bc69abae0951d
Matt Pieterse <63480247+mpiet-za@users.noreply.github.com>
2024-02-29 09:47:04 +0200

    Upload initial project state

---
+++ WordCounter-Console/Program.cs
@@ -0,0 +1,7 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("WordCounter (console/pre-0.1.0)\n-------------------------------");
+    }
+}
