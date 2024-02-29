18614d43319f4c717f0cd79ec8acb9b1d72fb4a5
Matt Pieterse <63480247+mpiet-za@users.noreply.github.com>
2024-02-29 10:07:30 +0200

    Add initial onboarding process

--- WordCounter-Console/Program.cs
+++ WordCounter-Console/Program.cs
@@ -1,7 +1,28 @@
-﻿internal class Program
+﻿using System.ComponentModel.Design;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("WordCounter (console/pre-0.1.0)\n-------------------------------");
+        Console.WriteLine("|---------------------------------|");
+        Console.WriteLine("| WordCounter (console/pre-0.1.0) |");
+        Console.WriteLine("|---------------------------------|\n");
+
+        Console.WriteLine("Enter your text to be parsed.");
+        Console.Write(">> ");
     }
+
+    static void Menu()
+    {
+        Console.Write('\n');
+        Console.WriteLine("1 : Get properties of text");
+        Console.WriteLine("0 : Exit application");
+        switch ((string) Console.ReadLine)
+        {
+
+        }
+        Console.Write('\n');
+    }
+
+    // 
 }
